c5931d7a8a79d2b4f8f580ae7d6eaf5019b7ae90
Jian Loong Liew <JianLoong@users.noreply.github.com>
2023-07-04 19:52:03 +1000

    Gh pages settings

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'aita/',
+  base: '/aita/',
   plugins: [react()],
-  build: {
-    outDir: './docs'
-  }
+  // build: {
+  //   outDir: './docs'
+  // }
 })
