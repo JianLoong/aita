73dff5e93baca2d33a619c9f65ce23f61ba20747
Jian Loong Liew <JianLoong@users.noreply.github.com>
2023-07-04 16:08:44 +1000

    Added dist for deployment

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -2,9 +2,11 @@
 
 export default function Introduction() {
   return (
-    <div className="p-2">
+    <div className="p-2 prose lg">
       <p>
-        This site periodically obtains data from Reddit and shows a summary of each post on the subreddit IATA.
+        This site periodically obtains data from Reddit and shows a summary of each post on the subreddit IATA. 
+
+        It currently has over 20,000 submissions and over 2 million top level comments.
       </p>
     </div>
   );
