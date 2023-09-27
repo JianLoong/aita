9827c6dd46de177b650d1ad8e1ca892cd4e6c887
Jian Liew <jianloongliew@gmail.com>
2023-09-27 14:52:48 +1000

    chore - updated base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/aita/',
+  base: '',
   plugins: [react()],
   // build: {
   //   outDir: './docs'
