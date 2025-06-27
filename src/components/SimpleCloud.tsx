968f13ee3d0d56d15c27111896a474fd59529f20
Jian Liew <jianloongliew@gmail.com>
2025-06-27 15:17:13 +1000

    Remove New Relic and adjust SimpleCloud rendering

--- src/components/SimpleCloud.tsx
+++ src/components/SimpleCloud.tsx
@@ -1,21 +1,23 @@
 import { TagCloud } from "react-tagcloud";
 
-export const SimpleCloud = (wordFrequency) => {
+export const SimpleCloud = ({ wordFrequency }) => {
 
     const darkMode = document.querySelector('html').getAttribute('data-theme') == "dark" ? true : false;
 
     const options = {
       luminosity: darkMode ? "light" : "dark",
-      hue: darkMode ? "blue" : "blue",
+      hue: darkMode ? undefined : "blue",
     };
 
-    const words = Object.keys(wordFrequency).map((key) => wordFrequency[key]);
+    if (!wordFrequency || wordFrequency.length === 0) {
+      return null; // Don't render if no word frequency data
+    }
 
     return (
       <TagCloud
         minSize={30}
         maxSize={60}
-        tags={words}
+        tags={wordFrequency}
         colorOptions={options}
       />
     );
