417308a3d13096fc5e46aad480c09113a0512cf3
Jian Liew <jianloongliew@gmail.com>
2026-01-02 09:00:44 +1100

    Add 2026 option to year dropdown in ViewTop

--- src/components/ViewTop.tsx
+++ src/components/ViewTop.tsx
@@ -221,6 +221,7 @@ export default function ViewTop() {
               <option value="2023">2023</option>
               <option value="2024">2024</option>
               <option value="2025">2025</option>
+              <option value="2026">2026</option>
             </select>
           </div>
         </div>
